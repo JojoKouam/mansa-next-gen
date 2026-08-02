1ca21a274565125231269051271ac986c34ddcf1
Josee Kouame <anabelle.kouame@epitech.eu>
2026-08-02 13:16:13 +0000

    [ADD] chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { BalanceCard } from "@/components/dashboard/BalanceCard";
+import { OverviewChart } from "@/components/dashboard/OverviewChart";
 import { TransactionList } from "@/components/dashboard/TransactionList";
 
 export default function Home() {
@@ -41,9 +42,14 @@ export default function Home() {
           ))}
         </div>
       </section>
-      <section>
-        <TransactionList />
-      </section>
+      <section className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div className="lg:col-span-2">
+          <OverviewChart />
+        </div>
+        <div className="lg:col-span-1">
+          <TransactionList />
+        </div>
+        </section>
 
       {/* Zone pour les futurs graphiques de l'étape 3 suite */}
       <section className="bg-mansa-anthracite/50 border border-white/5 rounded-4xl h-80 flex items-center justify-center italic text-gray-600">
